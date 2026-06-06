d09808c6c0a29ad19a8e719c2e307a8580010c0a
clickcookies <shumailabaripk@gmail.com>
2026-06-06 23:41:17 +0500

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -44,7 +44,7 @@ function renderFooter() {
           <h4 class="footer-col-title">Legal</h4>
           <ul>
             <li><a href="/privacy">Privacy Policy</a></li>
-            <li><a href="/terms">Terms of Service</a></li>
+            <li><a href="/terms">Terms of Use</a></li>
             <li><a href="/cookies-policy">Cookie Policy</a></li>
             <li><a href="/contact">Contact</a></li>
             <li><a href="/about">About</a></li>   
